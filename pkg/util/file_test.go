f6b221ff8bc0adc93d6648709e912fa1c3bc5aa8
Gareth Evans <gareth@bryncynfelin.co.uk>
2020-07-15 16:37:49 +0100

    chore: increased test coverage

---
+++ pkg/util/file_test.go
@@ -0,0 +1,26 @@
+package util_test
+
+import (
+	"github.com/plumming/chilly/pkg/util"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestConfigDir(t *testing.T) {
+	os.Setenv("HOME", "/random/home/dir")
+	dir := util.ConfigDir()
+	assert.Equal(t, "/random/home/dir/.chilly", dir)
+}
+
+func TestChillyConfigFile(t *testing.T) {
+	os.Setenv("HOME", "/random/home/dir")
+	dir := util.ChillyConfigFile()
+	assert.Equal(t, "/random/home/dir/.chilly/config.yml", dir)
+}
+
+func TestChillyGhConfigFile(t *testing.T) {
+	os.Setenv("HOME", "/random/home/dir")
+	dir := util.GhConfigDir()
+	assert.Equal(t, "/random/home/dir/.config/gh", dir)
+}
