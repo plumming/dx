af9217671f604cccd0994ccb27da34c4776a0a62
Gareth Evans <gareth@bryncynfelin.co.uk>
2020-07-24 17:01:30 +0100

    chore: remove unused code

--- pkg/util/color.go
+++ pkg/util/color.go
@@ -1,9 +1,6 @@
 package util
 
 import (
-	"errors"
-	"sort"
-
 	"github.com/fatih/color"
 )
 
@@ -30,79 +27,3 @@ var ColorBold = color.New(color.Bold).SprintFunc()
 // ColorAnswer returns a new function that returns answer-colorized (cyan) strings for the
 // given arguments with fmt.Sprint().
 var ColorAnswer = color.New(color.FgCyan).SprintFunc()
-
-var colorMap = map[string]color.Attribute{
-	// formatting
-	"bold":         color.Bold,
-	"faint":        color.Faint,
-	"italic":       color.Italic,
-	"underline":    color.Underline,
-	"blinkslow":    color.BlinkSlow,
-	"blinkrapid":   color.BlinkRapid,
-	"reversevideo": color.ReverseVideo,
-	"concealed":    color.Concealed,
-	"crossedout":   color.CrossedOut,
-
-	// Foreground text colors
-	"black":   color.FgBlack,
-	"red":     color.FgRed,
-	"green":   color.FgGreen,
-	"yellow":  color.FgYellow,
-	"blue":    color.FgBlue,
-	"magenta": color.FgMagenta,
-	"cyan":    color.FgCyan,
-	"white":   color.FgWhite,
-
-	// Foreground Hi-Intensity text colors
-	"hiblack":   color.FgHiBlack,
-	"hired":     color.FgHiRed,
-	"higreen":   color.FgHiGreen,
-	"hiyellow":  color.FgHiYellow,
-	"hiblue":    color.FgHiBlue,
-	"himagenta": color.FgHiMagenta,
-	"hicyan":    color.FgHiCyan,
-	"hiwhite":   color.FgHiWhite,
-
-	// Background text colors
-	"bgblack":   color.BgBlack,
-	"bgred":     color.BgRed,
-	"bggreen":   color.BgGreen,
-	"bgyellow":  color.BgYellow,
-	"BgBlue":    color.BgBlue,
-	"bgmagenta": color.BgMagenta,
-	"bgcyan":    color.BgCyan,
-	"bgwhite":   color.BgWhite,
-
-	// Background Hi-Intensity text colors
-	"bghiblack":   color.BgHiBlack,
-	"bghired":     color.BgHiRed,
-	"bghigreen":   color.BgHiGreen,
-	"bghiyellow":  color.BgHiYellow,
-	"bghiblue":    color.BgHiBlue,
-	"bghimagenta": color.BgHiMagenta,
-	"bghicyan":    color.BgHiCyan,
-	"bghiwhite":   color.BgHiWhite,
-}
-
-// GetColor returns the color for the list of colour names and option name.
-func GetColor(optionName string, colorNames []string) (*color.Color, error) {
-	attributes := []color.Attribute{}
-	for _, colorName := range colorNames {
-		a := colorMap[colorName]
-		if a == color.Attribute(0) {
-			return nil, errors.New("invalid option")
-		}
-		attributes = append(attributes, a)
-	}
-	return color.New(attributes...), nil
-}
-
-// ColorNameValues returns all the color names sorted.
-func ColorNameValues() []string {
-	answer := []string{}
-	for k := range colorMap {
-		answer = append(answer, k)
-	}
-	sort.Strings(answer)
-	return answer
-}
