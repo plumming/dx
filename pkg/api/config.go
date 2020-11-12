2b850be6439c33365db503338d9a484c3e2da456
cagiti <caicooper82@gmail.com>
2020-11-12 21:01:12 +0000

    fix: first step in supporting hosts in config.yml and in hosts.yml for gh cli

--- pkg/api/config.go
+++ pkg/api/config.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"path"
 
+	"github.com/pkg/errors"
+
 	"github.com/ghodss/yaml"
 	"github.com/plumming/dx/pkg/util"
 )
@@ -13,11 +15,15 @@ func ConfigFile() string {
 	return path.Join(util.GhConfigDir(), "config.yml")
 }
 
-func ParseDefaultConfig() (Config, error) {
-	return ParseConfig(ConfigFile())
+func HostsFile() string {
+	return path.Join(util.GhConfigDir(), "hosts.yml")
 }
 
-var ReadConfigFile = func(fn string) ([]byte, error) {
+func ParseDefaultConfig() (Config, error) {
+	return ParseConfig(ConfigFile(), HostsFile())
+}
+
+var readConfigFile = func(fn string) ([]byte, error) {
 	f, err := os.Open(fn)
 	if err != nil {
 		return nil, err
@@ -32,8 +38,8 @@ var ReadConfigFile = func(fn string) ([]byte, error) {
 	return data, nil
 }
 
-func parseConfigFile(fn string) ([]byte, Config, error) {
-	data, err := ReadConfigFile(fn)
+func parseConfigFile(fn, hf string) ([]byte, Config, error) {
+	data, err := readConfigFile(fn)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -44,11 +50,33 @@ func parseConfigFile(fn string) ([]byte, Config, error) {
 		return data, nil, err
 	}
 
+	// First step to support new hosts configuration in gh config
+	if root.Hosts == nil {
+		hosts, err := parseHostsFile(hf)
+		if err != nil {
+			return data, nil, err
+		}
+		root.Hosts = hosts
+	}
+
 	return data, &root, nil
 }
 
-func ParseConfig(fn string) (Config, error) {
-	_, root, err := parseConfigFile(fn)
+func parseHostsFile(hf string) (map[string]*HostConfig, error) {
+	var h map[string]*HostConfig
+	hostsFile, err := readConfigFile(hf)
+	if err != nil {
+		return h, err
+	}
+	err = yaml.Unmarshal(hostsFile, &h)
+	if err != nil {
+		return h, errors.Wrap(err, "while unmarshaling hosts file")
+	}
+	return h, nil
+}
+
+func ParseConfig(fn, hf string) (Config, error) {
+	_, root, err := parseConfigFile(fn, hf)
 	if err != nil {
 		return nil, err
 	}
--- pkg/api/config_test.go
+++ pkg/api/config_test.go
@@ -16,19 +16,62 @@ var (
       user: testuser
       oauth_token: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 `
+	emptyConfigFile = ""
+
+	dummyHostsFile = ` github.com:
+      user: testuser
+      oauth_token: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+`
+	emptyHostsFile = ""
 )
 
 func TestCanLoadConfigFromFile(t *testing.T) {
-	file, err := ioutil.TempFile(os.TempDir(), "TestCanLoadConfigFromFile")
+	configFile, err := ioutil.TempFile(os.TempDir(), "TestCanLoadConfigFromFile")
 	if err != nil {
 		panic(err)
 	}
-	defer os.Remove(file.Name())
+	hostsFile, err := ioutil.TempFile(os.TempDir(), "TestCanLoadConfigFromFile")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(configFile.Name())
+	defer os.Remove(hostsFile.Name())
 
-	err = ioutil.WriteFile(file.Name(), []byte(dummyConfigFile), 0600)
+	err = ioutil.WriteFile(configFile.Name(), []byte(dummyConfigFile), 0600)
 	assert.NoError(t, err)
 
-	config, err := api.ParseConfig(file.Name())
+	err = ioutil.WriteFile(hostsFile.Name(), []byte(emptyHostsFile), 0600)
+	assert.NoError(t, err)
+
+	config, err := api.ParseConfig(configFile.Name(), hostsFile.Name())
+	assert.NoError(t, err)
+
+	user := config.GetUser("github.com")
+	assert.Equal(t, "testuser", user)
+
+	token := config.GetToken("github.com")
+	assert.Equal(t, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", token)
+}
+
+func TestCanLoadEmptyConfigFromFile(t *testing.T) {
+	configFile, err := ioutil.TempFile(os.TempDir(), "TestCanLoadEmptyConfigFromFile")
+	if err != nil {
+		panic(err)
+	}
+	hostsFile, err := ioutil.TempFile(os.TempDir(), "TestCanLoadEmptyConfigFromFile")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(configFile.Name())
+	defer os.Remove(hostsFile.Name())
+
+	err = ioutil.WriteFile(configFile.Name(), []byte(emptyConfigFile), 0600)
+	assert.NoError(t, err)
+
+	err = ioutil.WriteFile(hostsFile.Name(), []byte(dummyHostsFile), 0600)
+	assert.NoError(t, err)
+
+	config, err := api.ParseConfig(configFile.Name(), hostsFile.Name())
 	assert.NoError(t, err)
 
 	user := config.GetUser("github.com")
