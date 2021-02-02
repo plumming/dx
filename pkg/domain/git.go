2af00f868bc44f04e77cf67d56cff47724dbb802
Gareth Evans <gareth@bryncynfelin.co.uk>
2021-02-02 11:55:35 +0000

    chore: add a check to validate origin & upstream

--- pkg/domain/git.go
+++ pkg/domain/git.go
@@ -6,6 +6,8 @@ import (
 	url2 "net/url"
 	"strings"
 
+	"github.com/jenkins-x/jx-logging/pkg/log"
+
 	"github.com/plumming/dx/pkg/util"
 )
 
@@ -20,6 +22,19 @@ func GetOrgAndRepoFromCurrentDir() (string, string, error) {
 	}
 
 	return ExtractOrgAndRepoFromGitRemotes(strings.NewReader(output))
+}
+
+func GetRemote(name string) (string, error) {
+	c := util.Command{
+		Name: "git",
+		Args: []string{"remote", "-v"},
+	}
+	output, err := c.RunWithoutRetry()
+	if err != nil {
+		return "", err
+	}
+
+	return ExtractURLFromRemote(strings.NewReader(output), name)
 }
 
 func CurrentBranchName(dir string) (string, error) {
@@ -149,26 +164,55 @@ func ConfigCommitterInformation(dir string, email string, name string) error {
 	return nil
 }
 
-func ExtractOrgAndRepoFromGitRemotes(reader io.Reader) (string, string, error) {
+func ExtractURLFromRemote(reader io.Reader, name string) (string, error) {
 	buf := new(strings.Builder)
 	_, err := io.Copy(buf, reader)
 	if err != nil {
-		return "", "", err
+		return "", err
 	}
 	lines := strings.Split(buf.String(), "\n")
-	for _, line := range lines {
-		if strings.HasPrefix(line, "origin") && strings.HasSuffix(line, "(push)") {
-			urlString := strings.TrimSpace(strings.ReplaceAll(strings.ReplaceAll(line, "origin\t", ""), "(push)", ""))
-			url, err := url2.Parse(urlString)
-			if err != nil {
-				return "", "", err
-			}
-			fragments := strings.Split(url.Path, "/")
-			if len(fragments) != 3 {
-				return "", "", errors.New("invalid url path '" + url.Path + "'")
-			}
-			return fragments[1], fragments[2], nil
+
+	log.Logger().Debugf("raw git remotes: %s", lines)
+
+	pushLines := filter(lines, func(line string) bool {
+		return strings.HasSuffix(line, "(push)")
+	})
+
+	log.Logger().Debugf("filtered git remotes: %s", pushLines)
+
+	for _, line := range pushLines {
+		if strings.HasPrefix(line, name) {
+			split := strings.Fields(line)
+			return split[1], nil
 		}
 	}
-	return "", "", errors.New("unable to find remote named 'origin'")
+
+	return "", errors.New("unable to find remote named '" + name + "'")
+}
+
+func ExtractOrgAndRepoFromGitRemotes(reader io.Reader) (string, string, error) {
+	urlString, err := ExtractURLFromRemote(reader, "origin")
+	if err != nil {
+		return "", "", errors.New("unable to find remote named 'origin'")
+	}
+
+	url, err := url2.Parse(urlString)
+	if err != nil {
+		return "", "", err
+	}
+
+	fragments := strings.Split(url.Path, "/")
+	if len(fragments) != 3 {
+		return "", "", errors.New("invalid url path '" + url.Path + "'")
+	}
+	return fragments[1], fragments[2], nil
+}
+
+func filter(in []string, test func(in string) bool) (out []string) {
+	for _, input := range in {
+		if test(input) {
+			out = append(out, input)
+		}
+	}
+	return
 }
